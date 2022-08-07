af9a85e68c9df8c8a52d17a139b2ce6631bb68b8
René Weiser <rene@encoding.group>
2022-08-07 17:08:28 +0200

    Changed project settings

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export {Calculator} from "./Calculator";
+export {Input} from "./Input";
+export {Addition} from "./Addition";
+export {Subtraction} from "./Subtraction";
+export {Multiplication} from "./Multiplication";
+export {Division} from "./Division";
